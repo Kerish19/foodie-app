f3d80d725072960f36cbde32f4c66ec4745776a4
Kerish <100kirill24@gmail.com>
2022-04-10 20:27:44 +0500

    header, menu

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,9 @@
 // import '~/app/libs/mmenu/dist/mmenu.js'
 
 document.addEventListener('DOMContentLoaded', () => {
-
-	// Custom JS
-
+	$('.header').after('<div class="mobile-menu"></div>');
+	$('.menu').clone().appendTo('.mobile-menu');
+	$('.mobile-menu-btn').click( function() {
+		$('.mobile-menu').toggleClass('active');
+	});
 })
